9d944517cfd8bb4800f8c256901b99076aa872da
tulusdeveloper <tulusdeveloper@gmail.com>
2024-06-12 22:24:14 +0300

    header updates

--- components/dashboard/SearchInput.jsx
+++ components/dashboard/SearchInput.jsx
@@ -14,8 +14,8 @@ export default function SearchInput() {
         <input
           type="text"
           id="simple-search"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search branch name..."
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 px-2 py-1.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search in Customers ..."
           required
         />
       </div>
