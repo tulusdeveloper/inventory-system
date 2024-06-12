6056d3a1ed3d04050928315fd7afa5a343c4f4e7
tulusdeveloper <tulusdeveloper@gmail.com>
2024-06-12 15:51:58 +0300

    install lucide-react

--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -1,9 +1,19 @@
+import { History } from 'lucide-react'
 import React from 'react'
 
 export default function Header() {
   return (
     <div className='bg-slate-300'>
-      <h2>Header is here</h2>
+      <div className='flex'>
+        {/* Recent Activities */}
+        <button>
+          <History/>
+        </button>
+         {/* Search */}
+      </div>
+      <div className="flex">
+         {/* */}
+      </div>
     </div>
   )
 }
